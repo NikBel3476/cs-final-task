a8610c25e1cc31f2a2e2e49e3e44a4c51c1b4832
NikBel3476 <nikita.belkin3476@gmail.com>
2022-12-23 22:25:26 +0400

    feature: complete task 3

---
+++ task3/Program.cs
@@ -0,0 +1,49 @@
+﻿while (true)
+{
+    var keyInfo = Console.ReadKey();
+    switch (keyInfo.Key)
+    {
+        case ConsoleKey.UpArrow:
+            Console.Clear();
+            Console.WriteLine("""  
+                ↑
+              ↑ ↑ ↑
+            ↑   ↑   ↑
+                ↑
+                ↑
+                ↑
+            """);
+            break;
+        case ConsoleKey.RightArrow:
+            Console.Clear();
+            Console.WriteLine("""
+                    →
+                      →
+            → → → → → → →
+                      →
+                    →
+            """);
+            break;
+        case ConsoleKey.DownArrow:
+            Console.Clear();
+            Console.WriteLine("""
+                ↓
+                ↓
+                ↓
+            ↓   ↓   ↓
+              ↓ ↓ ↓
+                ↓
+            """);
+            break;
+        case ConsoleKey.LeftArrow:
+            Console.Clear();
+            Console.WriteLine("""
+                ←
+              ←
+            ← ← ← ← ← ← ←
+              ←
+                ←
+            """);
+            break;
+    }
+}
