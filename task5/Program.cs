af4e6d72354382e66dba958340d62b869be17ce9
NikBel3476 <nikita.belkin3476@gmail.com>
2022-12-24 19:36:56 +0400

    feature: complete task 5

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("    |");
+for (int i = 1; i < 11; i++)
+{
+    Console.Write("{0, 4}", i);
+}
+Console.WriteLine();
+Console.WriteLine("—————————————————————————————————————————————");
+
+for (int i = 1; i < 11; i++)
+{
+    Console.Write("{0, 4}|", i);
+    for (int j = 1; j < 11; j++)
+    {
+        Console.Write("{0, 4}", i * j);
+    }
+    Console.WriteLine();
+}
