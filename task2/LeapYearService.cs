164e62dc6bdd105f2431ad14bc738f157d59219b
NikBel3476 <nikita.belkin3476@gmail.com>
2022-12-27 16:43:04 +0400

    fix: simplify IsLeapYearFunction

--- task2/LeapYearService.cs
+++ task2/LeapYearService.cs
@@ -1,18 +1,9 @@
 ﻿namespace task2;
 
-public class LeapYearService
+public static class LeapYearService
 {
     public static bool IsLeapYear(int year)
     {
-        if (year % 400 == 0)
-            return true;
-        
-        if (year % 100 == 0)
-            return false;
-
-        if (year % 4 == 0)
-            return true;
-
-        return false;
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 }
